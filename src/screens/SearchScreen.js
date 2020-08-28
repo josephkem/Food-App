b7b10579a6018578950a4691f527fcb0831bde3a
josephkem <josephkemoy@gmail.com>
2020-08-27 23:54:24 -0400

    Created  component for the search bar and added some basic styling

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { View, Text } from "react-native";
+import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
   return (
     <View>
+      <SearchBar />
       <Text>Screen Search</Text>
     </View>
   );
