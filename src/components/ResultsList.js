dc31e3c5312b914a4ca5402c19e3fbc5fa9c1d89
josephkem <josephkemoy@gmail.com>
2020-08-28 18:14:12 -0400

    Created a title property to be passed down to the results list component and added some basic styling to the property itself in the same component

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-const ResultsList = () => {
+const ResultsList = ({ title }) => {
   return (
     <View>
-      <Text>Results</Text>
+      <Text style={styles.titleStyle}>{title}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  titleStyle: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
 
 export default ResultsList;
