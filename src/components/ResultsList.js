beb3625f7f8f5f11859dca4657ff175f1d40b1fb
josephkem <josephkemoy@gmail.com>
2020-08-28 18:41:52 -0400

    Created a flatlist in the result component to render by name

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 
 const ResultsList = ({ title, results }) => {
   return (
     <View>
       <Text style={styles.titleStyle}>{title}</Text>
-      <Text>Results: {results.length}</Text>
+      <FlatList
+        keyExtractor={(result) => result.id}
+        horizontal={true}
+        data={results}
+        renderItem={({ item }) => {
+          return <Text>{item.name}</Text>;
+        }}
+      />
     </View>
   );
 };
