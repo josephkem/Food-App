dc7952a0d79249834acce67b7206316b5793452a
josephkem <josephkemoy@gmail.com>
2020-08-28 19:26:07 -0400

    Added styling to multiple components to make the layout neater and user friendly

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -5,7 +5,7 @@ import ResultsDetail from "../components/ResultsDetail";
 
 const ResultsList = ({ title, results }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.titleStyle}>{title}</Text>
       <FlatList
         keyExtractor={(result) => result.id}
@@ -23,6 +23,11 @@ const styles = StyleSheet.create({
   titleStyle: {
     fontSize: 18,
     fontWeight: "bold",
+    marginLeft: 15,
+    marginBottom: 5,
+  },
+  container: {
+    marginBottom: 10,
   },
 });
 
