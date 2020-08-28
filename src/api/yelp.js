d04e19e0f0dc33ee6f938bc1c0e359703fcd0f54
josephkem <josephkemoy@gmail.com>
2020-08-28 09:16:19 -0400

    Fixed an error which cause a failed http request

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
     Authorization:
-      "Bearer benj0TMs0kPYkOZEoVa3hud4wPzhNXVgtehgaSlKyubYfbU4obLisprHYFUF6z00mbNx6Sm7Bp6GjHSy-496nSCXfEBKgyIgSZAABcqEbC3Rj9A32p0LQQuURu5",
+      "Bearer benj0TMs0kPYkOZEoVa3hud4wPzhNXVgtehgaSlKyubYfbU4obLisprHYFUF6z00mbNx6Sm7Bp6GjHSy-496nSCXfEBKgyIgSZAABcqEbC3Rj9A32p0LQQuURu5HX3Yx",
   },
 });
