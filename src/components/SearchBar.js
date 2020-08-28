b44008b7ff29fffddbbf8fa7eec40014b5b0468b
josephkem <josephkemoy@gmail.com>
2020-08-28 00:15:46 -0400

    Added more styling to the search bar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,32 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 const SearchBar = () => {
   return (
-    <View style={styles.background}>
-      <Text>Search </Text>
+    <View style={styles.backgroundStyle}>
+      <Feather name="search" style={styles.iconStyle} />
+      <TextInput style={styles.inputStyle} placeholder="Search" />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  background: {
+  backgroundStyle: {
     backgroundColor: "#F0EEEE",
     height: 50,
     borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: "row",
+    marginTop: 15,
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 20,
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
     marginHorizontal: 15,
   },
 });
