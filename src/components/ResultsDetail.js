ac303363a29430ffaffc9a1ee407a7a5fc71ebe6
josephkem <josephkemoy@gmail.com>
2020-08-28 18:49:54 -0400

    Created a separate component to render the details of the individual results inside the results ccomponent

---
+++ src/components/ResultsDetail.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+
+const ResultsDetail = () => {
+  return (
+    <View>
+      <Text>Detail</Text>
+    </View>
+  );
+};
+
+export default ResultsDetail;
