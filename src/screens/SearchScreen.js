0e0179e101b5a33f261fb9c5ed0bc1a53df8ecf9
josephkem <josephkemoy@gmail.com>
2020-08-28 17:53:31 -0400

    Created a separate directory to create hooks for the application and transferred the hook used for the results of the search

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,31 +2,11 @@ import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import SearchBar from "../components/SearchBar";
 import yelp from "../api/yelp";
+import useResuts from "../hooks/useResults";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const searchApi = async (searchTerm) => {
-    try {
-      const response = await yelp.get("/search", {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: "san jose",
-        },
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      setErrorMessage("Something went wrong");
-    }
-  };
-
-  //Call search api when component is rendered
-  useEffect(() => {
-    searchApi("pasta");
-  }, []);
+  const [searchApi, results, errorMessage] = useResuts();
 
   return (
     <View>
